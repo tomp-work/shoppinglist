de0861f15679a1524cf19bf88891b61adf4b5278
u5tdp <tomp.work.email@gmail.com>
2026-02-06 13:12:20 +0000

    story2: add shopping list item: FE

---
+++ src/AddForm.tsx
@@ -0,0 +1,90 @@
+import { Button, Form, Input, InputNumber, type FormProps } from 'antd';
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import './App.css'
+
+type Item = {
+  id?: string;
+  name: string;
+  quantity: number;
+}
+
+type FieldType = {
+  name?: string;
+  quantity?: string;
+};
+
+export default function AddForm() {
+
+  const queryClient = useQueryClient();
+
+  const createItem = async (data: Item) => {
+    const response = await fetch('http://localhost:1323/item', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error('Failed to create item');
+    }
+    return response.json();
+  };
+
+  const createMutation = useMutation({
+    mutationFn: (data: Item) => createItem(data),
+    onSuccess: (data) => {
+      console.log("Item created:", data);
+      queryClient.invalidateQueries({ queryKey: ["items"] });
+    },
+    onError: (error) => {
+      console.error("Error creating item:", error);
+    },
+  });
+
+  const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+    console.log('Success:', JSON.stringify(values));
+    createMutation.mutate({
+      name: values.name ?? '',
+      quantity: (values.quantity ?? 0) as number,
+    });
+  };
+
+  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      initialValues={{
+        quantity: 1
+      }}
+      layout='inline'
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item<FieldType>
+        label="Name"
+        name="name"
+        rules={[{ required: true, message: 'Please input item name!' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item<FieldType>
+        label="Quantity"
+        name="quantity"
+        rules={[{ required: true, message: 'Please input item quantity!' }]}
+      >
+        <InputNumber />
+      </Form.Item>
+      <Form.Item label={null}>
+        <Button disabled={createMutation.isPending} type="primary" htmlType="submit" >
+          Add
+        </Button>
+      </Form.Item>
+      {createMutation.isError && <p>Something went wrong with create</p>}
+      {createMutation.isSuccess && <p>Item created successfully!</p>}
+    </Form >
+  )
+}
--- src/ShoppingList.tsx
+++ src/ShoppingList.tsx
@@ -1,52 +1,52 @@
-import { Table } from 'antd';
+import { Card, Divider, Table } from 'antd';
 import { useQuery } from '@tanstack/react-query'
+import AddForm from './AddForm.tsx'
 import './App.css'
 
 const getItemList = async () => {
-  const res = await fetch("http://localhost:1323/item");
-  if (!res.ok) {
-    console.log('Failed to fetch list items');
-    throw new Error('Failed to fetch list items');
-  }
-  return res.json();
+    const res = await fetch("http://localhost:1323/item");
+    if (!res.ok) {
+        console.log('Failed to fetch list items');
+        throw new Error('Failed to fetch list items');
+    }
+    return res.json();
 };
 
 export default function ShoppingList() {
-  const { isPending, error, data } = useQuery({
-    queryKey: ['items'],
-    queryFn: () => {
-      return getItemList();
+    const { isPending, error, data } = useQuery({
+        queryKey: ['items'],
+        queryFn: () => {
+            return getItemList();
+        }
+    });
+
+    // TODO: Improve FE status/error handling.
+    if (isPending) {
+        return 'Loading...';
     }
-  });
+    if (error) {
+        return 'An error has occurred: ' + error.message;
+    }
 
-  // TODO: Improve FE status/error handling.
-  if (isPending) {
-    return 'Loading...';
-  }
-  if (error) {
-    return 'An error has occurred: ' + error.message;
-  }
+    // AntD table column definition for shopping list table.
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Quantity',
+            dataIndex: 'quantity',
+            key: 'quantity',
+        },
+    ];
 
-  // AntD table column definition for shopping list table.
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Quantity',
-      dataIndex: 'quantity',
-      key: 'quantity',
-    },
-  ];
-
-  return (
-    <>
-      <h1>Shopping List</h1>
-      <div className="card">
-        <Table rowKey="id" dataSource={data} columns={columns} />
-      </div>
-    </>
-  )
+    return (
+        <Card title="Shopping List">
+            <AddForm />
+            <Divider />
+            <Table bordered rowKey="id" dataSource={data} columns={columns} />
+        </Card>
+    )
 }
